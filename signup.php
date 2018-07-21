76e58840d326390e64462e5341f44fde0c09a0d8
AishwaryaAishuS <41518636+AishwaryaAishuS@users.noreply.github.com>
2018-07-21 10:18:56 -0700

    Add files via upload

---
+++ signup.php
@@ -0,0 +1,46 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<body>
+<table width="300" border="2" align="center" >
+<tr>
+<form action="action_page.php" style="border:1px solid #ccc">
+  <div class="container">
+    <h1>Sign Up</h1>
+    <p>Please fill in this form to create an account.</p>
+    <hr>
+
+    <label for="email"><b>Email</b></label>
+    <input type="text" placeholder="Enter Email" name="email" required>
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required>
+
+    <label for="psw-repeat"><b>Repeat Password</b></label>
+    <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
+
+    <label>
+      <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
+    </label>
+
+    <p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
+
+    <div class="clearfix">
+      <button type="button" class="cancelbtn">Cancel</button>
+      <button type="submit" class="signupbtn">Sign Up</button>
+	  <form>
+	  <button onclick="success.location.href="success.php">signup</button>
+	  <tr>
+	  <table>
+	  <td>
+
+     </tr>
+	 </table>
+	 </td>
+	  </form>
+    </div>
+  </div>
+</for
+</tr>
+</table>                                 
+</body>
+</html>
